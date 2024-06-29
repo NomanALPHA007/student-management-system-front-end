07131bcd7955e38e65b53c3509fd1eceec1114c3
Ehtasham Al Noman <ehtasham@graduate.utm.my>
2024-06-29 20:17:41 +0800

    1st commit
    Updated login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,71 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [admin, setAdmin] = useState({username: "", password: ""});
-    const [errMessage, setErrMessage] = useState("");
-    const navigate = useNavigate();
+  const [admin, setAdmin] = useState({ username: "", password: "" });
+  const [errMessage, setErrMessage] = useState("");
+  const navigate = useNavigate();
 
-    function handleChange(event) {
-        const {name, value} = event.target;
-        setAdmin(prevState => {
-            return {
-                ...prevState,
-                [name]: value
-            }
-        });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setAdmin((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleLogin(event) {
+    event.preventDefault();
+    if (admin.username !== "admin") {
+      setErrMessage("Username is not correct");
+      document.getElementById("username").focus();
+      return;
+    } else if (admin.password !== "admin123") {
+      setErrMessage("Password is not correct");
+      document.getElementById("password").focus();
+      return;
     }
+    navigate("/dashboard");
+  }
 
-    function handleLogin(event) {
-        event.preventDefault();
-        if (admin.username !== "admin") {
-            setErrMessage("Username is not correct");
-            document.getElementById("username").focus();
-            return;
-        } else if (admin.password !== "admin123") {
-            setErrMessage("Password is not correct");
-            document.getElementById("password").focus();
-            return;
-        }
-        navigate("/dashboard");
-    }
-
-    return (
-        <div className={"centered-element"}>
-            <img className={"login-img"} src={"./images/login-logo.png"} width={"120px"} alt={"login-logo"}/>
-            <div className={"login-container"}>
-                <h1>Admin Login</h1>
-                <br/>
-                <form onSubmit={handleLogin} className={"login-form"}>
-                    <input onChange={handleChange} id={"username"} type={"text"} name={"username"} placeholder={"Username"} value={admin.username}/>
-                    <input onChange={handleChange} id={"password"} type={"password"} name={"password"} placeholder={"Password"} value={admin.password}/>
-                    <button type={"submit"}>Login</button>
-                </form>
-                <h5>{errMessage}</h5>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"centered-element"}>
+      <img
+        className={"login-img"}
+        src={"./images/login-logo.png"}
+        width={"120px"}
+        alt={"login-logo"}
+      />
+      <div className={"login-container"}>
+        <h1>Admin Login</h1>
+        <br />
+        <form onSubmit={handleLogin} className={"login-form"}>
+          <input
+            onChange={handleChange}
+            id={"username"}
+            type={"text"}
+            name={"username"}
+            placeholder={"Username"}
+            value={admin.username}
+          />
+          <input
+            onChange={handleChange}
+            id={"password"}
+            type={"password"}
+            name={"password"}
+            placeholder={"Password"}
+            value={admin.password}
+          />
+          <button type={"submit"}>Login</button>
+        </form>
+        <h5>{errMessage}</h5>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
+
+//Added code
